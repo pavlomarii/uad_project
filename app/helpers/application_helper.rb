eeda2cfd88a1b3df1c27bf762d62375825982464
Pavlo Marii <pavlomarii@gmail.com>
2023-05-08 14:09:53 +0300

    Add avatar to navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,10 @@
 module ApplicationHelper
-  def avatar
+  def avatar(variant)
     if current_user.blank? || !current_user.avatar.attached?
-      image_tag "default_avatar.png"
+      map_of_sizes = { thumb: 256, small: 32 }
+      image_tag "default_avatar_#{map_of_sizes[variant]}.png"
     else
-      image_tag current_user.avatar.variant(:thumb), class: "rounded"
+      image_tag current_user.avatar.variant(variant), class: "rounded"
     end
   end
 end
