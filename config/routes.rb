cd00b488094d6bfb7e39647e359783977c5168ab
Pavlo Marii <pavlomarii@gmail.com>
2023-05-08 16:21:32 +0300

    Add letter opener for development

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
   root "dashboard#index"
 
   # Define routes for user authentication.
