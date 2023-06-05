b1f691ad06b68376b1a72a96a65df4d3af9f07db
Pavlo Marii <pavlomarii@gmail.com>
2023-06-05 13:17:01 +0300

    Add profile displaying for all users

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -47,13 +47,14 @@ class ArticlesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_article
-      @article = Article.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def article_params
-      params.require(:article).permit(:title, :content).merge(author: current_user)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def article_params
+    params.require(:article).permit(:title, :content).merge(author: current_user)
+  end
 end
